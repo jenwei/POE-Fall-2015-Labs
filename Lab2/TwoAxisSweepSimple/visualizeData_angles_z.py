4529e9c1d8bf89b4d509e221ebb1fcfbecddb187
Dennis Chen <dennis.chen@students.olin.edu>
2015-09-22 15:20:54 -0400

    Changed angles_z to work with new data format

--- Lab2/TwoAxisSweepSimple/visualizeData_angles_z.py
+++ Lab2/TwoAxisSweepSimple/visualizeData_angles_z.py
@@ -7,6 +7,8 @@ def analogReadToDist(a):
     return voltageToDist(analogReadToVoltage(a))
 
 def voltageToDist(v):
+    if(v == 0):
+        return 200
     return (np.log(v) - np.log(4.2223)) / -0.024
     
 def analogReadToVoltage(a):
@@ -20,7 +22,7 @@ def polarToCoordinate(a, d):
 def getArrayFromFile(path):
     return np.loadtxt(path, delimiter=',')
 
-def plotScannerData(data):
+def plotScannerData(x,y,z):
     width, height = np.shape(data)
     X = np.arange(0,height,1)
     Y = np.arange(0,width,1)
@@ -28,11 +30,14 @@ def plotScannerData(data):
     Z = data
     hf = plt.figure()
     ha = hf.add_subplot(111, projection='3d')
-    ha.plot_surface(X,Y,Z, cmap=cm.coolwarm, cstride=2)
+    ha.plot_surface(x,y,z, cmap=cm.coolwarm, cstride=2)
     ha.set_zlabel('distance from scanner (cm)')
     plt.show()
 
 if __name__ == '__main__':
-    analogReadVals = getArrayFromFile('scanner3Ddata.csv')
-    data = np.vectorize(analogReadToDist)(analogReadVals)     
-    plotScannerData(data)
+    data = getArrayFromFile('data_09222015.csv')
+    X = np.reshape(data[:,0], (60, -1))
+    Y = np.reshape(data[:,1], (60, -1))
+    analogReadVals = np.reshape(data[:,2], (60, -1))
+    Z = np.vectorize(analogReadToDist)(analogReadVals)     
+    plotScannerData(X,Y,Z)
