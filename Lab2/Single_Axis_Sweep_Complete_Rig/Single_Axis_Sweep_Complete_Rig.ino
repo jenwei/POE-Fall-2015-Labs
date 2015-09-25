554a239a8b3e76d03a096e9bbb617e8a944d0061
Jennifer Wei <jennifer.wei@students.olin.edu>
2015-09-25 14:49:55 -0400

    ADD: code for single sweep for the completed rig

---
+++ Lab2/Single_Axis_Sweep_Complete_Rig/Single_Axis_Sweep_Complete_Rig.ino
@@ -0,0 +1,39 @@
+#include <Servo.h> 
+
+const int HORZ_SWEEP_START = 80;
+const int HORZ_SWEEP_RANGE = 60;
+const int VERT_SWEEP_START = 40;
+const int VERT_SWEEP_RANGE = 60;
+const int SETTLE_TIME = 200;
+
+Servo horz;  
+Servo vert;
+ 
+void setup() 
+{ 
+  horz.attach(10);
+  vert.attach(9);
+  Serial.begin(9600);
+  runScan();
+} 
+
+void runScan()
+{
+  for(int j = HORZ_SWEEP_START; j < HORZ_SWEEP_START + HORZ_SWEEP_RANGE; j++){
+    horz.write(j);
+    delay(SETTLE_TIME);
+    int sensorValue = analogRead(0); 
+    Serial.print(j);
+    Serial.print(",");
+    Serial.print(sensorValue);
+    Serial.print(",");
+    Serial.print("\n");
+  }  
+}
+
+void loop()
+{ 
+  /**
+   leaving this here because it won't compile otherwise 
+   */
+ }
